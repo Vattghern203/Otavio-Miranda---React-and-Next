a2f92697a25d05db51b388ea31519a314c5c67d0
Otávio Augusto <otavioaugustobrb19@gmail.com>
2023-01-11 11:23:05 -0300

    working with map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "title 1",
+        body: "body 1",
+      },
+
+      {
+        id: 2,
+        title: "title 2",
+        body: "body 2",
+      },
+
+      {
+        id: 3,
+        title: "title 3",
+        body: "body 3",
+      },
+    ]   
+  }
+
+  render() {
+
+    const { posts } = this.state
+
+    return (
+      <div className="App">
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+            <p>{post.title}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
